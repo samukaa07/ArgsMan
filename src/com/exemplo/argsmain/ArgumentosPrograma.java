506b717c1452d979376151da02e20fd1d92e1fb6
samukaa07 <Samuel_pereira_lima@hotmail.com>
2022-06-02 19:47:29 -0300

    Initial commit

---
+++ src/com/exemplo/argsmain/ArgumentosPrograma.java
@@ -0,0 +1,20 @@
+package com.exemplo.argsmain;
+
+public class ArgumentosPrograma {
+
+	// criando argumentos " " na guia -> run -> runconfigurations -> (x) argumentos e inserir " "  com a quantidade de args
+	public static void main(String [] args) {
+					
+		
+		
+		args[0] = "Samuel";
+		args[1] = "Dev Java";
+		args[2] = "Teste";
+		
+		System.out.println("O primeiro elemento é: " + args[0]);
+		System.out.println("O segundo elemento é: " + args[1]);
+		System.out.println("O segundo elemento é: " + args[2]);
+			
+	}
+
+}
---
+++ src/com/exemplo/argsmain/ArgumentosPrograma2.java
@@ -0,0 +1,16 @@
+package com.exemplo.argsmain;
+
+public class ArgumentosPrograma2 {
+	
+	static String argumento[] = new String[] {"Samuel", "DEV", "100"};
+	
+
+	public static void main(String[] args) {
+		
+		// String[] args = {"Samuel", "Dev", "100};
+			System.out.println(argumento.length);
+			
+			
+	}
+
+}
